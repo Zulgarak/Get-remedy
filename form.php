f01511f50f51b7fb0d7383e307cf2930bf9e2e6c
Oleg Maksimchik <olegmr1991@gmail.com>
2024-06-27 18:45:46 +0300

    feat(*): test access

--- form.php
+++ form.php
@@ -4,8 +4,8 @@ $phone = $_POST['phone'];
 $text .= "Имя: $name \n";
 $text .= "Телефон: $phone \n\n";
 
-  $file = fopen ("order.txt", "a+");
-  fwrite ($file,$text);
-  fclose ($file);
+$file = fopen ("order.txt", "a+");
+fwrite ($file,$text);
+fclose ($file);
 
 ?>
